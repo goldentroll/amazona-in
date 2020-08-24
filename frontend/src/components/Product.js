3471a602f14c6751efac3b5f41c6c2e247ab109b
goldentroll <Kcjjos@outlook.com>
2020-08-24 17:46:01 +0430

    Video-08-Create-Rating-Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Rating from './Rating';
 
 export default function Product(props) {
   const { product } = props;
@@ -16,7 +17,10 @@ export default function Product(props) {
       <div className="product-brand">{product.brand}</div>
       <div className="product-price">${product.price}</div>
       <div className="product-rating">
-        {product.rating} stars ({product.numReviews} reviews)
+        <Rating
+          value={product.rating}
+          text={`${product.numReviews} reviews`}
+        ></Rating>
       </div>
     </div>
   );
